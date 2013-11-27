32f7925748cc7e83c091151e7a71b0cd042a0649
Jimmy Liu <jimmyliu03@gmail.com>
2013-11-27 02:59:01 -0600

    v8

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131127052023) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "statuses", force: true do |t|
     t.string   "name"
     t.text     "content"
